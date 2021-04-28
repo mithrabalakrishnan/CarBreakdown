53f5af72c8030bd65c455b8656bc4ee0fe139223
mithrabalakrishnan <mithrabalakrishnan555@gmail.com>
2021-04-29 00:33:01 +0530

    Fixed Bugs and Ui Changes

--- app/src/main/java/uk/ac/tees/aad/W9511357/Faq.java
+++ app/src/main/java/uk/ac/tees/aad/W9511357/Faq.java
@@ -22,8 +22,24 @@ public class Faq extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.listViewid);
 
-        String[] q ={"How to use app?","is login mandetory?","need to pay to use app?","Does it require GPS?","Do we track location?"} ;
-        String[] a ={"Login and register with app to use","Yes","no need of any payment","Yes we use Gps to get your location","No, in case of help we get your location"} ;
+        String[] q ={"How to use app?",
+                "is login mandetory?",
+                "need to pay to use app?",
+                "Does it require GPS?",
+                "Do we track location?",
+                "What should I do if i get Flat tyre?",
+                "What do the warinig lights on my car mean?",
+                "what do i do if i put a wrong fuel in my car?"} ;
+
+        String[] a ={"Login and register with app to use",
+                "Yes",
+                "no need of any payment",
+                "Yes we use Gps to get your location",
+                "No, in case of help we get your location",
+                "in most cases, aflat tyre will mean a tyre change is required and can be fixed with a puncture repair kit",
+                "When a warning light illuminates on your dashboard, it is a sign of a fault with your vechile",
+                "You should call recover company, turning on tha car makes loss of engine performence."
+        } ;
 
         FaqAdapter adapter = new FaqAdapter(getApplicationContext(), q,  a);
         listView.setAdapter(adapter);
